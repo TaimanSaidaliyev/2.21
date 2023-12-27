6b072ab6a35da7736b9cdad508d3961b24078991
nazerke.zhanbolatova@atikz.com <taimansaidaliev@gmail.com>
2023-12-27 22:33:10 +0600

    first commit

--- Posts/views.py
+++ Posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def show_list_posts(request):
+    template ='Posts/posts_list.html'
+    posts_list = Post.objects.all()
+    context = {
+        'posts_list': posts_list
+    }
+    return render(request, template, context)
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from Posts.views import show_list_posts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', show_list_posts)
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
